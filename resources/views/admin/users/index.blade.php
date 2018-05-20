35f40b0f227b76ae0f0929cc673e074456d8371f
Denis <sidni@i.ua>
2018-05-20 12:51:03 +0300

    display pagination

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -30,6 +30,7 @@
                             @endforeach
                         </tbody>
                     </table>
+                    {{ $users->links() }}
                 </div>
             </div>
         </div>
